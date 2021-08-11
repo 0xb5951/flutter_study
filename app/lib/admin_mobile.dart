b35c37cf3ba5fbc6693bebc9ffde3cfacf64aaac
odrum428 <oxkeita@gmail.com>
2021-08-11 14:34:24 +0900

    add: SideBar

--- app/lib/admin_mobile.dart
+++ app/lib/admin_mobile.dart
@@ -10,8 +10,36 @@ class _AdminMobilePageState extends State<AdminMobilePage> {
   Widget build(Object context) {
     return Scaffold(
       body: Row(
-        children: [Text("test")],
+        children: [SideBar()],
       ),
+    );
+  }
+}
+
+class SideBar extends StatefulWidget {
+  @override
+  _SideBar createState() => _SideBar();
+}
+
+class _SideBar extends State<SideBar> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        selectedIndex = index;
+      },
+      destinations: [
+        NavigationRailDestination(
+            icon: Icon(Icons.people), label: Text("People")),
+        NavigationRailDestination(
+            icon: Icon(Icons.thumbs_up_down), label: Text("Thunb up down")),
+        NavigationRailDestination(icon: Icon(Icons.face), label: Text("Face")),
+        NavigationRailDestination(
+            icon: Icon(Icons.bookmark), label: Text("bookmark")),
+      ],
     );
   }
 }
