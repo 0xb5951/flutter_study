98e7a18a42afd4dca39e9864ab1db7d77fb77665
odrum428 <oxkeita@gmail.com>
2021-08-11 13:35:09 +0900

    add: HomeRoutePage:

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,7 +12,32 @@ class TodoApp extends StatelessWidget {
       // テーマカラー
       theme: ThemeData(primarySwatch: Colors.blue),
       // Todoリスト一覧画面を表示
-      home: TodoListPage(),
+      home: HomeRoutePage(),
+      routes: <String, WidgetBuilder>{
+        'main': (BuildContext context) => new HomeRoutePage(),
+        'todos': (BuildContext context) => new TodoListPage(),
+      },
+    );
+  }
+}
+
+class HomeRoutePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("メインルート画面")),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text("TODOリスト"),
+              onTap: () {
+                Navigator.of(context).pushNamed('todos');
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
