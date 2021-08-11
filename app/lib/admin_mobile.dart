b14b6d63d2687dd1482ea3962ed44242d8c1decd
odrum428 <oxkeita@gmail.com>
2021-08-11 15:08:14 +0900

    add: _PostHeader

--- app/lib/admin_mobile.dart
+++ app/lib/admin_mobile.dart
@@ -27,7 +27,9 @@ class _SideBar extends State<SideBar> {
   @override
   Widget build(BuildContext context) {
     return NavigationRail(
+      // 現在選択されている要素のインデックス
       selectedIndex: selectedIndex,
+      // 要素がタップされたときに呼び出される
       onDestinationSelected: (index) {
         selectedIndex = index;
       },
@@ -39,6 +41,55 @@ class _SideBar extends State<SideBar> {
         NavigationRailDestination(icon: Icon(Icons.face), label: Text("Face")),
         NavigationRailDestination(
             icon: Icon(Icons.bookmark), label: Text("bookmark")),
+      ],
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // 要素にぶつかるまでいっぱいに広げる。Row, Columnで使う
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            title: Text("Post"),
+            subtitle: Text("20 Post"),
+            leading: ClipOval(
+              //子要素をくり抜く
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.storage,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            title: Text("ALL types"),
+            subtitle: Text(""),
+            leading: ClipOval(
+              //子要素をくり抜く
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.style,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
