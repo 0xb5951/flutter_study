36b7c68061534bb4f6355fa8622d4d487e03a53f
odrum428 <oxkeita@gmail.com>
2021-08-17 14:10:21 +0900

    update: 注釈を追加

--- app/lib/admin_mobile.dart
+++ app/lib/admin_mobile.dart
@@ -9,8 +9,18 @@ class _AdminMobilePageState extends State<AdminMobilePage> {
   @override
   Widget build(Object context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin Mobile"),
+      ),
       body: Row(
-        children: [SideBar()],
+        children: [
+          SideBar(),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            // このExpandedがないとすべて表示できない
+            child: PostList(),
+          ),
+        ],
       ),
     );
   }
@@ -46,38 +56,65 @@ class _SideBar extends State<SideBar> {
   }
 }
 
-class _PostHeader extends StatelessWidget {
+class PostList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // 画面上部に対するpadding
+      padding: EdgeInsets.only(top: 16),
+      child: Column(
+        // 要素を縦に並べたいのでcolumn
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _PostsHeader(),
+          Expanded(
+            child: ListView(
+              children: [
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PostsHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      // 要素を横に並べたいのでrow
       children: [
-        // 要素にぶつかるまでいっぱいに広げる。Row, Columnで使う
         Expanded(
-          flex: 1,
+          flex: 1, // このExpandedが占める比率。複数Expandedがある時に使う
           child: ListTile(
-            title: Text("Post"),
-            subtitle: Text("20 Post"),
             leading: ClipOval(
-              //子要素をくり抜く
               child: Container(
-                color: Colors.grey[300],
+                //このコンテナだけだと、四角いアイコンになる
+                color: Colors.grey[300], // []内は色の濃さ。999までで高いほど黒くなる
                 width: 48,
                 height: 48,
                 child: Icon(
+                  //要素の真ん中にストレージアイコンが表示される
                   Icons.storage,
-                  color: Colors.grey[800],
+                  color: Colors.grey[800], // アイコンの色も指定できる
                 ),
               ),
             ),
+            title: const Text('Posts'),
+            subtitle: const Text('20 Posts'),
           ),
         ),
         Expanded(
           flex: 1,
           child: ListTile(
-            title: Text("ALL types"),
-            subtitle: Text(""),
             leading: ClipOval(
-              //子要素をくり抜く
               child: Container(
                 color: Colors.grey[300],
                 width: 48,
@@ -88,9 +125,176 @@ class _PostHeader extends StatelessWidget {
                 ),
               ),
             ),
+            title: const Text('All Types'),
+            subtitle: const Text(''),
           ),
         ),
       ],
+    );
+  }
+}
+
+// このクラスを作ることで、UI要素を使いまわしている
+class _Post extends StatelessWidget {
+  final String name;
+  final String message;
+  final String textReason;
+  final Color colorPrimary;
+  final Color colorPositive;
+  final String textPositive;
+  final Color colorNegative;
+  final String textNegative;
+
+  // このクラスで使う引数を定義
+  const _Post({
+    Key? key,
+    required this.name,
+    required this.message,
+    required this.textReason,
+    required this.colorPrimary,
+    required this.colorPositive,
+    required this.textPositive,
+    required this.colorNegative,
+    required this.textNegative,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // それぞれのカードの余白を調整する
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      // ここから下で一つのカード要素を表しているが、長くてイマイチ分からない
+      child: Card(
+        elevation: 8,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          // カード要素の子要素
+          children: [
+            // カードのアイコンと上部のテキスト２つ
+            ListTile(
+              leading: ClipOval(
+                child: Container(
+                  color: colorPrimary,
+                  width: 48, // アイコンのサイズ
+                  height: 48,
+                  child: Center(
+                    // アイコンの中の文字。名前の先頭一文字を抜き出している
+                    child: Text(
+                      name.substring(0, 1),
+                      style: const TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(name),
+              subtitle: Text('2 min ago'),
+            ),
+            // 真ん中の円とメッセージ部分
+            Container(
+              // ここも要素右のpadding.sizedboxとの使い分けがわからない
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // メッセージ部分の左paddingを行っている
+                  const SizedBox(width: 72),
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      // 要素のボーダーを太くして色をつけている
+                      border: Border.all(color: colorPrimary, width: 4),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  // 丸図形とテキストの間のpadding
+                  const SizedBox(width: 8),
+                  Flexible(child: Text(message)),
+                ],
+              ),
+            ),
+            // 下のtextReasonとボタン群
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  // textReason要素
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: colorPrimary, width: 2),
+                      ),
+                    ),
+                    child: Text(
+                      textReason,
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  // textReasonとネガティブボタンの間padding
+                  const SizedBox(width: 24),
+                  // ここにExpandedすることでtextReasonの長さに左右されなくしている
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: colorNegative,
+                      ),
+                      onPressed: () {},
+                      child: Text(textNegative),
+                    ),
+                  ),
+                  // ボタン間のpadding
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: colorPositive,
+                        backgroundColor: colorPositive.withOpacity(0.2),
+                      ),
+                      onPressed: () {},
+                      child: Text(textPositive),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PostGreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const _Post(
+      name: 'Name',
+      message: 'message',
+      textReason: 'textReason',
+      colorPrimary: Colors.greenAccent, // アイコンなどの色
+      colorPositive: Colors.greenAccent,
+      textPositive: 'Positive',
+      colorNegative: Colors.blueAccent,
+      textNegative: 'Negative',
+    );
+  }
+}
+
+class _PostRed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const _Post(
+      name: 'Namaga Tema',
+      message: 'Not recomended for publication.',
+      textReason: 'Pending Reason',
+      colorPrimary: Colors.deepOrangeAccent,
+      colorPositive: Colors.blueAccent,
+      textPositive: 'Publish',
+      colorNegative: Colors.deepOrangeAccent,
+      textNegative: 'Decline',
     );
   }
 }
