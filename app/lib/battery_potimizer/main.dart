fb208e9bd90dc91ea15900e0bdfadaf13ce501bc
odrum428 <oxkeita@gmail.com>
2021-08-23 23:41:36 +0900

    add: battery Indicator部分のベースを実装

--- app/lib/battery_potimizer/main.dart
+++ app/lib/battery_potimizer/main.dart
@@ -9,7 +9,7 @@ const bColorTitle = Color(0xFF616161);
 const bColorText = Color(0xFF9E9E9E);
 const bElevation = 4.0;
 
-// ignore: use_key_in_widget_constructors
+// ignore:
 class BatteryOptimizerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,6 +25,7 @@ class BatteryOptimizerPage extends StatelessWidget {
           child: Column(
             children: [
               const OptimizerButtons(),
+              const BatteryLevelIndicator(),
             ],
           ),
         ),
@@ -41,7 +42,7 @@ class OptimizerButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
+      scrollDirection: Axis.horizontal, // これで横スクロールする
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8),
         child: Row(
@@ -63,7 +64,7 @@ class OptimizerButtons extends StatelessWidget {
 }
 
 class _OptimizerButton extends StatelessWidget {
-  final String title;
+  final String title; // ボタンに表示する文字
 
   const _OptimizerButton({
     Key? key,
@@ -72,18 +73,49 @@ class _OptimizerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var styleFrom = ElevatedButton.styleFrom(
+      primary: Colors.white, // ボタンの背景
+      padding: const EdgeInsets.symmetric(horizontal: 16), // これがどこに聞いているか謎
+      shape: RoundedRectangleBorder(
+        // ボタンの周りの形
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
     return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: Colors.white,
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-        ),
+        style: styleFrom,
         onPressed: () {},
         child: Text(
           title,
           style: const TextStyle(color: bColorTitle, fontSize: 12),
         ));
+  }
+}
+
+class _BatteryLevelIndicatorPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    final c = Offset(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(c, 80, paint);
+  }
+
+  @override
+  bool shouldRepaint(_BatteryLevelIndicatorPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(_BatteryLevelIndicatorPainter oldDelegate) =>
+      false;
+}
+
+class BatteryLevelIndicator extends StatelessWidget {
+  const BatteryLevelIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _BatteryLevelIndicatorPainter(),
+      size: Size(400, 400),
+    );
   }
 }
