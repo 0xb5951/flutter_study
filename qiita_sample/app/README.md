b2549e4559d28de3b04d61fafa6f29490904fd40
odrum428 <oxkeita@gmail.com>
2021-09-05 16:39:18 +0900

    init: 投稿一覧画面のベースを作成

--- qiita_sample/app/README.md
+++ qiita_sample/app/README.md
@@ -3,9 +3,56 @@ UIの練習でFlutterの肌間が分かってきたので、次はもうちょ�
 以下のページを参考にQiitaが閲覧できるサービスを作ってみる。
 https://www.flutter-study.dev/create-app/qiita-app
 
+## 概要
+Qiitaの記事一覧を
+
+## 要件
+- Qiitaの記事一覧を確認できる
+- Qiitaにログインできる
+- 記事一覧から詳細を確認できる
+- ユーザごとのプロフィールを確認できる
+- ログアウトできる
 
 ## 設計
 ### Qiita APIについて
+![image](https://user-images.githubusercontent.com/33741792/131454938-c8cfffaa-0ad8-4433-a8f6-2d81292d97ed.png)
 
+
+### ログイン周りの実装
+![image](https://user-images.githubusercontent.com/33741792/131505440-17b112a3-9fff-4d2b-8750-0d4c2ee644fb.png)
+
+1. Qiitaログイン用のURLをWebブラウザで開く
+ブラウザでログイン用のURLが開くので、各自のユーザでログインする
+2. リダイレクトURLからアクセストークン発行コードを受け取る
+3. アクセストークン発行APIに発行コードを付与してリクエスト
+4. アクセストークンを受け取りローカルに保存
+5. 投稿一覧画面に遷移
+
+ログインページを実装して、そこにロジックを載せていく。
+![image](https://user-images.githubusercontent.com/33741792/131508441-a1b10f39-28ce-4a81-a66f-8d0815897bc7.png)
+
+Qiitaのリダイレクト先URLにディープリンクを設定する。
+ディープリンクの設定は、Android, iOSそれぞれの設定内で行う。
+
+Androidの場合は以下のように設定する。
+```
+<activity>
+    ...
+    <intent-filter>
+        <action android:name="android.intent.action.VIEW" />
+        <category android:name="android.intent.category.DEFAULT" />
+        <category android:name="android.intent.category.BROWSABLE" />
+        <data
+            android:scheme="qiitaapp"
+            android:host="dev.flutterstudy.qiitaapp" />
+    </intent-filter>
+</activity>
+```
+
+この場合、このアプリを認識するためのリンクは以下になる。
+qiitaapp://dev.flutterstudy.qiitaapp
 
 ### tips
+- ExpandedとButton組み合わせると、Buttonの要素がExpandedされるので、間に適当なWidgetを入れると良し
+- url_launcherを使って、webブラウザを起動する
+- 秘匿情報の管理にはflutter_dotenvを使う
---
+++ qiita_sample/app/lib/screen/item_list.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class ItemListScreen extends StatefulWidget {
+  @override
+  _ItemListScreenState createState() => _ItemListScreenState();
+}
+
+class _ItemListScreenState extends State<ItemListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Qiita App'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                // 押したら、自分のプロフィールに遷移
+              },
+              // あとでログインユーザのプロフィールに遷移
+              icon: const Icon(Icons.local_movies),
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _PostedItem(
+                  title: 'title',
+                  userName: 'user_name',
+                  postedDate: DateTime.now(),
+                  profileUrl:
+                      'https://avatars.githubusercontent.com/u/33741792?s=88&u=fdfb8ec0ee5ad4ea0f7a4b2c7f0a6c7ede6581a8&v=4',
+                ),
+                _PostedItem(
+                  title: 'title',
+                  userName: 'user_name',
+                  postedDate: DateTime.now(),
+                  profileUrl:
+                      'https://s3-ap-northeast-1.amazonaws.com/qiita-image-store/0/1075243/04b9772c31531b440303a986812c5795ed587b23/large.png?1612246089',
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Now() {}
+}
+
+// 一つの投稿要素を生成する
+class _PostedItem extends StatelessWidget {
+  final String title;
+  final String userName;
+  final DateTime postedDate;
+  // あとプロフィールの画像もある
+  final String profileUrl;
+
+  const _PostedItem({
+    Key? key,
+    required this.title,
+    required this.userName,
+    required this.postedDate,
+    required this.profileUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {}, // 押したら、その投稿に移動する
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 16),
+      ),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.network(profileUrl),
+      ),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // これで両端に寄せる
+        children: [
+          Text(userName),
+          Text('5/31 10:55'),
+        ],
+      ),
+      trailing: Container(
+        height: 24,
+        width: 24,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          '5',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
