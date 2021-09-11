13d495aaaae7da8c62c3d7209b1427c579cbae76
odrum428 <oxkeita@gmail.com>
2021-09-11 21:35:51 +0900

    update: 関数ごとにgruopでくくる

--- qiita_sample/app/test/component/url_component_test.dart
+++ qiita_sample/app/test/component/url_component_test.dart
@@ -5,18 +5,22 @@ import 'package:app/component/url_component.dart';
 
 void main() {
   UrlComponent urlComponent = UrlComponent();
-  test('入力文字数が等しいか', () {
-    expect(urlComponent.randomString(10).length, 10);
-  });
 
-  test('意図した文字以外が使われていないか', () {
-    const String expectedValue = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    String generatedString = urlComponent.randomString(10);
-    expect(generatedString.replaceAll(RegExp(r'[' + expectedValue + r']'), ''),
-        '');
-  });
+  group('randomString()', () {
+    test('入力文字数が等しいか', () {
+      expect(urlComponent.randomString(10).length, 10);
+    });
 
-  test('入力文字数が0のときは、何も出力されない', () {
-    expect(urlComponent.randomString(0), '');
+    test('意図した文字以外が使われていないか', () {
+      const String expectedValue = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      String generatedString = urlComponent.randomString(10);
+      expect(
+          generatedString.replaceAll(RegExp(r'[' + expectedValue + r']'), ''),
+          '');
+    });
+
+    test('入力文字数が0のときは、何も出力されない', () {
+      expect(urlComponent.randomString(0), '');
+    });
   });
 }
