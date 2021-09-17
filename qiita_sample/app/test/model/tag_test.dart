fc12dd151b4fc8c0e69274ac92496cfdbae86242
odrum428 <oxkeita@gmail.com>
2021-09-18 01:27:05 +0900

    update: 複数件あったときのテストを追加

--- qiita_sample/app/test/model/tag_test.dart
+++ qiita_sample/app/test/model/tag_test.dart
@@ -14,5 +14,30 @@ void main() {
       expect(tag.name, 'qiita');
       expect(tag.versions, ['0.0.1']);
     });
+
+    test('複数件あったときに、正しく変換できるか', () {
+      final expectedJson = {
+        "tags": [
+          {
+            "name": "qiita",
+            "versions": ["0.0.1"],
+          },
+          {
+            "name": "sample",
+            "versions": ["0.0.1"],
+          },
+        ],
+      };
+
+      final tagList = (expectedJson['tags'] as List<dynamic>).map((tagJson) {
+        return Tag.fromJson(tagJson);
+      }).toList();
+
+      expect(tagList.length, 2);
+      expect(tagList[0].name, "qiita");
+      expect(tagList[0].versions, ["0.0.1"]);
+      expect(tagList[1].name, "sample");
+      expect(tagList[1].versions, ["0.0.1"]);
+    });
   });
 }
