edbf762afcb3f5286e6a28dbab7d28d6f994f0fd
odrum428 <oxkeita@gmail.com>
2021-09-18 17:44:12 +0900

    init: flutter create

---
+++ bloc_pattarn/playbook/file/run_second_startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    sudo usermod -u 1000 -o circleci
+    sudo groupmod -o -g 1000 circleci
+    sudo chown -R circleci:circleci .gnupg/
+    while true; do sleep 1000; done
+else
+    echo "-- Not first container startup --"
+    cd /home/circleci/app && flutter pub get
+    sudo chown circleci -R /dev/kvm
+fi
